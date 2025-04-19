2311d52fcab7da5b6efc6e74d32446cf03494f98
eirayfq <ynmiss66@gmail.com>
2025-04-19 14:01:54 +0800

    提交项目

---
+++ eaps/src/main/java/com/fq/yznu/eaps/EapsApplication.java
@@ -0,0 +1,13 @@
+package com.fq.yznu.eaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EapsApplication.class, args);
+	}
+
+}
